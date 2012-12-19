952f9d0258cd71676d4b4f620fce8cbbe9b81c91
Kyle Hardgrave <kyle@kylehardgrave.com>
2012-12-18 19:49:24 -0500

    Basic centrality working.

--- centrality.py
+++ centrality.py
@@ -10,7 +10,7 @@ def centrality(vects):
 
     # For each vector, find the average similarity to all the other
     # vectors. Use reference equality to avoid comparing with self.
-    return [(sum([sim(vect, vect1)
+    return [(sum([cosine_sim(vect, vect1)
                   for vect1 in vects
                   if vect is not vect1])
              / n)
@@ -21,22 +21,23 @@ def gen_summary(sents, max_words):
     """Given a list of tokenized sentences and a threshold summary
     length (in words), return an ordered list of sentences comprising
     the summary."""
-    vects = map(binary_vectorize, sents)
+    feat_space = sorted(set().union(*sents)) # TODO: Remove funcwords, etc?
+    vects = [binary_vectorize(feat_space, sent) for sent in  sents]
     centralities = sorted(zip(centrality(vects), sents), reverse=True)
     summary = []
     word_count = 0
 
-    for sent, score in centralities:
+    for score, sent in centralities:
         if word_count >= max_words:
             break
         if is_valid_sent_len(sent) and not is_repeat(sent, summary):
             summary.append(sent)
             word_count += len(sent)
 
-    assert sum(map(len, summary)) <= max_words, 'Summary not within threshold'
+#    assert sum(map(len, summary)) <= max_words, 'Summary not within threshold'
     return summary
 
 
 if __name__ == '__main__':
-    collection_sents = get_toks(ls(INPUT_ROOT)[0])
-    print collection_sents
+    sents = get_toks(ls(INPUT_ROOT)[0])
+    print gen_summary(sents, 100)
--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 """Common utilities for working with text, etc."""
 
 import os
+import math
 from nltk import tokenize
 
 
@@ -53,28 +54,6 @@ def get_toks(path):
     return [tokenize.word_tokenize(sent) for sent in get_sentences(path)]
 
 
-# Summarizer utils
-# ----------------
-
-def is_valid_sent_len(sent, min_len=MIN_SENT_LEN, max_len=MAX_SENT_LEN):
-    """Takes a list of tokens, returns if valid token length."""
-    return min_len <= len(sent) <= max_len
-
-
-def is_repeat(sent, sents, vect_fun=binary_vectorize, max_sim=MAX_SIM_CUTOFF):
-    """Given a tokenized sentence and a list of tokenized sentences,
-    return whether the sentences overlaps too highly in content with any
-    of the others."""
-    # TODO: Incorporate synonyms to better discern similarity
-    for other_sent in sents:
-        feat_space = feature_space(sent, other_sent)
-        x, y = vect_fun(feat_space, sent), vect_fun(feat_space, other_sent)
-        if cosine_sim(x, y) > max_sim:
-            return True
-    return False
-
-
-
 # Vectors and similarities
 # ------------------------
 
@@ -87,7 +66,8 @@ def cosine_sim(x, y):
     assert len(x) == len(y), 'Vectors are not the same length.'
     zipped = zip(x, y)
     top = float(sum(v * w for v, w in zipped))
-    bot = sqrt(sum(pow(v, 2) for v in x)) * sqrt(sum(pow(w, 2) for w in y))
+    bot = (math.sqrt(sum(pow(v, 2) for v in x))
+           * math.sqrt(sum(pow(w, 2) for w in y)))
     try:
         return top / bot
     except ZeroDivisionError:
@@ -111,3 +91,25 @@ def tfidf_vectorize(feature_space, doc):
 def feature_space(doc1, doc2):
     """Given two lists of tokens, return a common feature set."""
     return sorted(set(doc1) | set(doc2))
+
+
+# Summarizer utils
+# ----------------
+
+def is_valid_sent_len(sent, min_len=MIN_SENT_LEN, max_len=MAX_SENT_LEN):
+    """Takes a list of tokens, returns if valid token length."""
+    return min_len <= len(sent) <= max_len
+
+
+def is_repeat(sent, sents, vect_fun=binary_vectorize, max_sim=MAX_SIM_CUTOFF):
+    """Given a tokenized sentence and a list of tokenized sentences,
+    return whether the sentences overlaps too highly in content with any
+    of the others."""
+    # TODO: Incorporate synonyms to better discern similarity
+    for other_sent in sents:
+        feat_space = feature_space(sent, other_sent)
+        x, y = vect_fun(feat_space, sent), vect_fun(feat_space, other_sent)
+        if cosine_sim(x, y) > max_sim:
+            return True
+    return False
+
